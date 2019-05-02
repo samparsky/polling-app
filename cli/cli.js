49418455539dfb1d9363fcf1d9a826d76cc31a0c
Omidiora Samuel <8148384+samparsky@users.noreply.github.com>
2019-05-02 04:34:57 +0100

    update: functions and cli

--- cli/cli.js
+++ cli/cli.js
@@ -1,22 +1,258 @@
 #!/usr/bin/env node
 
 const program = require("commander")
+const colors = require('colors')
+const assert = require('assert')
 
 program
   .version('0.1.0')
-  .command('create_organisation <dir>')
-  .command('add_constituent')
-  .command('remove_constituent')
-  .command('add_initiative')
-  .command('vote')
-  .command('get_result')
+  .option('-n, --chain <chain>', 'default: ethereum', 'ethereum')
+  .option('-p, --httpProvider <provider>', 'http provider', 'http://localhost:8545')
+  .option('-w, --websocketProvider <provider>', 'websocket provider', 'ws://localhost:8545')
+  .option('-k, --keystore <dir>', 'keystore location')
+  .option('-p, --password <password>', 'keystore password')
+program
+  .command('create_organisation')
+  .description('create an organisation')
+  .action(createOrganisation)
+program
+  .command('add_constituent <orgId> <constituentAddress>')
+  .description('add a constituent to an organisation')
+  .action(addConstituent)
+program
+  .command('remove_constituent <orgId> <constituentAddress>')
+  .description('remove a constituent from an organisation')
+  .action(removeConstituent)
+program
+  .command('add_initiative <orgId> <initiative>')
+  .description('add an initiative to an organisation')
+  .action(addInitiative)
+program
+  .command('vote <orgId> <initiativeId> <choice>')
+  .description('vote for an initiative')
+  .action(vote)
+program
+  .command('get_result <orgId> <initiativeId>')
+  .description('get result for an initiative')
+  .action(getInitiativeResult)
+
+const contractAddress = '0x564540a26Fb667306b3aBdCB4ead35BEb88698ab'
+const lib = require('./lib')( program.httpProvider, program.websocketProvider, contractAddress)
+
+program.parse(process.argv);
+
+function logError(e){
+  console.log(`\n${colors.red(e.toString())}\n`)
+  process.exit()
+}
+
+async function createOrganisation() {
+  const { privateKey, address } = await lib.getAccount(program).catch(logError)
+
+  const tx = await lib.createOrganisation({ 
+    contractAddress, 
+    account: address, 
+    network: '*',
+    provider: program.httpProvider
+  })
+
+  console.log(colors.green(`✔ Succesfully created the transaction`))
+
+  const { rawTransaction, transactionHash } = await lib.signTransaction({ tx, privateKey})
+  await lib.broadcast({ signedTx: rawTransaction })
+
+  console.log(colors.green(`✔ Succesfully broadcasted the transaction`))
+  console.log(colors.gray(`check transaction status at \nhttps://rinkeby.etherscan.io/tx/${transactionHash}.. `))
+
+  const displayResult = (event) => {
+    const { organisationId } = event.returnValues
+    console.log(colors.green('✓ Succesfully created organisation'))
+    console.log(colors.green(`\nOrganisation ID = ${organisationId.toString()}\n`))
+  }
+
+  // watch for mined event and exit
+  await lib.subscribeEvent('CreateOrganisation', { creator: address }, displayResult, logError, transactionHash)
+}
+
+async function addConstituent(orgId, constituentAddress, args){
+  const { privateKey, address } = await lib.getAccount(program).catch(logError)
+
+  const tx = await lib.addConstituent({ 
+    orgId,
+    constituentAddress: constituentAddress.split(','),
+    contractAddress, 
+    account: address, 
+    network: '*',
+    provider: program.httpProvider
+  })
+
+  console.log(colors.green(`✔ Succesfully created the transaction`))
+
+  const { rawTransaction, transactionHash } = await lib.signTransaction({ tx, privateKey })
+  await lib.broadcast({ signedTx: rawTransaction })
+
+  console.log(colors.green(`✔ Succesfully broadcasted the transaction`))
+  console.log(colors.gray(`check transaction status at \nhttps://rinkeby.etherscan.io/tx/${transactionHash}.. `))
+
+  const displayResult = (event) => {
+    const { organisationId, constituents } = event.returnValues
+    console.log(colors.green(`✓ Succesfully added constituent(s) to oragnisation ${orgId}`))
+    console.log(colors.green(`\nOrganisation ID = ${organisationId.toString()}\n`))
+    console.log(colors.green(`\nConstituents    = ${constituents.toString()}\n`))
+  }
+   // watch for mined event and exit
+   await lib.subscribeEvent('AddConstituent', { organisationId: orgId }, displayResult, logError, transactionHash)
+
+}
+
+async function removeConstituent(orgId, constituentAddress, args){
+  const { privateKey, address } = await lib.getAccount(program).catch(logError)
+
+  const tx = await lib.removeConstituent({ 
+    orgId,
+    constituentAddress,
+    contractAddress, 
+    account: address, 
+    network: '*',
+    provider: program.httpProvider
+  })
+
+  console.log(colors.green(`✔ Succesfully created the transaction`))
+
+  const { rawTransaction, transactionHash } = await lib.signTransaction({ tx, privateKey })
+  await lib.broadcast({ signedTx: rawTransaction })
+
+  console.log(colors.green(`✔ Succesfully broadcasted the transaction`))
+  console.log(colors.gray(`check transaction status at \nhttps://rinkeby.etherscan.io/tx/${transactionHash}.. `))
+
+  const displayResult = (event) => {
+    const { organisationId, constituent } = event.returnValues
+    console.log(colors.green(`✓ Succesfully removed constituent from oragnisation ${orgId}`))
+    console.log(colors.green(`\nOrganisation ID = ${organisationId.toString()}`))
+    console.log(colors.green(`\nConstituent   = ${constituent}\n`))
+  }
+   // watch for mined event and exit
+   await lib.subscribeEvent('RemoveConstituent', { organisationId: orgId }, displayResult, logError, transactionHash)
 
 
+}
+/**
+ * 
 
-function createOrganisation() {
-  // const tx = //
+ { initiativeTitle: 'one', ballotOptions: [1,2,3,4], expiryTime: 1556700062459 }
+ */
+
+async function addInitiative(orgId, initiative, args){
+  const { privateKey, address } = await lib.getAccount(program).catch(logError)
+
+  const {
+    initiativeTitle, 
+    ballotOptions,
+    expiryTime, 
+    number_of_votes_allowed=0, 
+    allowAnyOne=false,
+  } = JSON.parse(initiative)
   
-  // sign 
+  assert.ok(orgId)
+  assert.ok(initiativeTitle)
+  assert.ok(Array.isArray(ballotOptions))
+  assert.ok(expiryTime > 0)
 
-  // broadcast
+  const tx = await lib.addInitiative({
+    orgId, 
+    initiativeTitle, 
+    ballotOptions, 
+    expiryTime, 
+    number_of_votes_allowed, 
+    allowAnyOne,
+    contractAddress, 
+    account: address, 
+    network: '*',
+    provider: program.httpProvider
+  })
+
+  console.log(colors.green(`✔ Succesfully created the transaction`))
+
+  const { rawTransaction, transactionHash } = await lib.signTransaction({ tx, privateKey })
+  await lib.broadcast({ signedTx: rawTransaction })
+
+  console.log(colors.green(`✔ Succesfully broadcasted the transaction`))
+  console.log(colors.gray(`check transaction status at \nhttps://rinkeby.etherscan.io/tx/${transactionHash}.. `))
+
+  const displayResult = (event) => {
+    const { organisationId, initiativeId,initiativeTitle, expiryTime, number_of_votes_allowed, _ballotOptions } = event.returnValues
+    console.log(colors.green(`✓ Succesfully created initiative for oragnisation ${orgId}`))
+    console.table(
+      [{ 
+        organisationId: organisationId.toNumber(), 
+        initiativeId: initiativeId.toNumber(), 
+        initiativeTitle, 
+        expiryTime: expiryTime.toNumber(), 
+        number_of_votes_allowed: number_of_votes_allowed.toNumber() , 
+        ballotOptions: _ballotOptions.map(item => item.toNumber()).toString()  }]  )
+    // console.log(colors.green(`\nConstituent   = ${constituent}\n`))
+  }
+
+  // watch for mined event and exit
+  await lib.subscribeEvent('AddInitiative', { organisationId: orgId }, displayResult, logError, transactionHash)
+
+}
+
+async function vote(orgId, initiativeId, choice, args){
+  const { privateKey, address } = await lib.getAccount(program).catch(logError)
+
+  const tx = await lib.vote({
+    orgId, 
+    initiativeId, 
+    choice,
+    contractAddress, 
+    account: address, 
+    network: '*',
+    provider: program.httpProvider
+  })
+
+  console.log(colors.green(`✔ Succesfully created the transaction`))
+
+  const { rawTransaction, transactionHash } = await lib.signTransaction({ tx, privateKey })
+  await lib.broadcast({ signedTx: rawTransaction })
+
+  console.log(colors.green(`✔ Succesfully broadcasted the transaction`))
+  console.log(colors.gray(`check transaction status at \nhttps://rinkeby.etherscan.io/tx/${transactionHash}.. `))
+  const displayResult = (event) => {
+    const { organisationId, voter, success } = event.returnValues
+    console.log(colors.green(`✓ Succesfully voted for initiative ${initiativeId} in organisation ${orgId}`))
+    console.table([
+      {
+        organisationId, voter, success
+      }
+    ])
+  }
+   // watch for mined event and exit
+   await lib.subscribeEvent('Vote', { organisationId: orgId, voter: address }, displayResult, logError, transactionHash)
+
+}
+
+async function getInitiativeResult(orgId, initiativeId, args) {
+  const { address } = await lib.getAccount(program).catch(logError)
+  // loading icon
+  console.log(colors.green(` Getting initiative result`))
+  
+  const result = await lib.getInitiativeResult({
+    orgId, 
+    initiativeId, 
+    contractAddress, 
+    account: address, 
+    network: '*',
+    provider: program.httpProvider
+  })
+  // display result in table format
+  const votes = result['0'].map((choice, index) => new Vote(choice.toNumber(), result['1'][index].toNumber()))
+
+  console.table(votes)
+  process.exit()
+}
+
+function Vote(choice, vote){
+  this.choice = choice
+  this.vote = vote
 }
--- cli/lib.js
+++ cli/lib.js
@@ -1,22 +1,200 @@
-const { Contract, ContractFactory, Wallet} = require('ethers')
 const Web3 = require('web3')
-const web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/<REDACTED API KEY"));
+const assert = require('assert')
+const fetch = require('node-fetch')
+const abi = require('./Polling.json');
+const Spinner = require('cli-spinner').Spinner;
 
-module.exports = {
 
-    createOrganisation: function({ account, contractAddress }){
-        const nonce = web3.eth.getTransactionCount(account)
-        const gasPrice = web3.at
+module.exports = function( httpProvider, websocketProvider, contractAddress ) {
+    const web3 = new Web3(new Web3.providers.HttpProvider(httpProvider))
+    const wsWeb3 =  new Web3(new Web3.providers.WebsocketProvider(websocketProvider))
+    const pollingContract = new wsWeb3.eth.Contract(abi, contractAddress)
+
+    return {
+
+    signTransaction: async function({ privateKey, tx }) {
+        const { rawTransaction } =  await web3.eth.accounts.signTransaction(tx, privateKey)
+        const  transactionHash = web3.utils.sha3(rawTransaction, { encoding: "hex" });
+
+        return { rawTransaction, transactionHash }
+    },
+
+    subscribeEvent: async function(event, filter, display, logError, transactionHash ) {
+        const spinner = new Spinner(`\n %s Waiting for transaction to be mined..\n`);
+        spinner.start();
+      
+        pollingContract.events[event]({
+            filter,
+        }, function(error, event) {
+            spinner.stop();
+            spinner.clearLine()net
+
+            if(error){
+                logError(error)
+            } else {
+                display(event)
+            }
+
+            process.exit()
+        })
+    },
+
+    getAccount: async function ({ keystore, password, provider }){
+        const web3 = new Web3(new Web3.providers.HttpProvider(provider))
+        // load keystore json file
+        const loadKeystore = require(keystore)
+        return web3.eth.accounts.decrypt(loadKeystore, password)
+    },      
+
+    createOrganisation: async function({ account, contractAddress, provider }){
+        const Web3 = require('web3')
+        const web3 = new Web3(new Web3.providers.HttpProvider(provider))
+
+        // get gas price
+        const { fast } = await fetch('https://ethgasstation.info/json/ethgasAPI.json').then(res => res.json())
+        const nonce = await web3.eth.getTransactionCount(account)
+        const pollingContract = new web3.eth.Contract(abi, contractAddress)
+        const calldata = await pollingContract.methods.createOrganisation().encodeABI()
+        const gasLimit = await pollingContract.methods.createOrganisation().estimateGas()
+
         return {
-            contractAddress,
             nonce,
-            gasPrice: '0x09184e72a000', 
-            gasLimit: '0x2710',
-            to: '0x0000000000000000000000000000000000000000', 
+            gasPrice: fast,
+            gasLimit,
+            to: contractAddress,
             value: '0x00', 
-            data: '0x7f7465737432000000000000000000000000000000000000000000000000000000600057',
-            // EIP 155 chainId - mainnet: 1, ropsten: 3
-            chainId: 3
+            data: calldata,
+            chainId: 1337
         }
+
+    },
+
+    addConstituent: async function({ orgId, constituentAddress, account, contractAddress, provider}) {
+        const { fast } = await fetch('https://ethgasstation.info/json/ethgasAPI.json').then(res => res.json())
+        const nonce = await web3.eth.getTransactionCount(account)
+        const pollingContract = new web3.eth.Contract(abi, contractAddress)
+        const calldata = await pollingContract.methods.addConstituent(orgId, constituentAddress).encodeABI()
+        const gasLimit = await pollingContract.methods.addConstituent(orgId, constituentAddress).estimateGas()
+
+        return {
+            nonce,
+            gasPrice: fast,
+            gasLimit,
+            to: contractAddress,
+            value: '0x00', 
+            data: calldata,
+            chainId: 1337
+        }
+    },
+    removeConstituent: async function({ orgId, constituentAddress, account, contractAddress, provider='ropsten'}) {
+        const { fast } = await fetch('https://ethgasstation.info/json/ethgasAPI.json').then(res => res.json())
+        const nonce = await web3.eth.getTransactionCount(account)
+        const pollingContract = new web3.eth.Contract(abi, contractAddress)
+        const calldata = await pollingContract.methods.removeConstituent(orgId, constituentAddress).encodeABI()
+        const gasLimit = await pollingContract.methods.removeConstituent(orgId, constituentAddress).estimateGas()
+
+        return {
+            nonce,
+            gasPrice: fast,
+            gasLimit,
+            to: contractAddress,
+            value: '0x00', 
+            data: calldata,
+            chainId: 1337
+        }
+    },
+    addInitiative: async function({
+        orgId, 
+        initiativeTitle, 
+        ballotOptions, 
+        expiryTime, 
+        number_of_votes_allowed, 
+        allowAnyOne,
+        contractAddress, 
+        account, 
+        provider
+      }) {
+        const { fast } = await fetch('https://ethgasstation.info/json/ethgasAPI.json').then(res => res.json())
+        const nonce = await web3.eth.getTransactionCount(account)
+        const pollingContract = new web3.eth.Contract(abi, contractAddress, { gasPrice: fast, defaultAccount: account })
+        const calldata = await pollingContract.methods.addInitiative(
+            orgId, 
+            initiativeTitle, 
+            ballotOptions, 
+            expiryTime, 
+            number_of_votes_allowed, 
+            allowAnyOne
+        ).encodeABI()
+
+        const gasLimit = await pollingContract.methods.addInitiative(
+            orgId, 
+            initiativeTitle, 
+            ballotOptions, 
+            expiryTime, 
+            number_of_votes_allowed, 
+            allowAnyOne
+        ).estimateGas()
+
+        return {
+            nonce,
+            gasPrice: fast,
+            gasLimit,
+            to: contractAddress,
+            value: '0x00', 
+            data: calldata,
+            chainId: 1337
+        }
+
+    },
+    vote: async function({
+        orgId, 
+        initiativeId,
+        choice,
+        contractAddress, 
+        account, 
+        provider
+    }) {
+        const { fast } = await fetch('https://ethgasstation.info/json/ethgasAPI.json').then(res => res.json())
+        const nonce = await web3.eth.getTransactionCount(account)
+        const pollingContract = new web3.eth.Contract(abi, contractAddress, { gasPrice: fast, defaultAccount: account })
+        const calldata = await pollingContract.methods.vote(orgId, initiativeId, choice).encodeABI()
+        const gasLimit = await pollingContract.methods.vote(orgId, initiativeId, choice).estimateGas()
+
+        return {
+            nonce,
+            gasPrice: fast,
+            gasLimit,
+            to: contractAddress,
+            value: '0x00', 
+            data: calldata,
+            chainId: 1337
+        }
+    },
+    getInitiativeResult: async function({
+        orgId, 
+        initiativeId,
+        contractAddress, 
+        account, 
+        provider
+    }) {
+        const { fast } = await fetch('https://ethgasstation.info/json/ethgasAPI.json').then(res => res.json())
+        const pollingContract = new web3.eth.Contract(abi, contractAddress, { gasPrice: fast, defaultAccount: account })
+        const result = await pollingContract.methods.getInitiativeResult(orgId, initiativeId).call()
+        return result
+    },
+
+    broadcast: async function({ signedTx, provider}) {
+        const Web3 = require('web3')
+        const web3 = new Web3(new Web3.providers.HttpProvider(provider))
+
+        // assert
+        assert.ok(signedTx.startsWith('0x'), 'signed transaction should start with 0x')
+
+        await web3.eth.sendSignedTransaction(signedTx)
+        .on('error', function(err){
+            console.error(err)
+            process.exit()
+        })
     }
+}
 }
